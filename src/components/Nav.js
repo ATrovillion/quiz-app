ca35352a676f06822e853faad6a3e6e40a834a35
Aaron Trovillion <aaron.trovillion@gmail.com>
2023-09-19 13:58:40 -0400

    Built out basic structure of project

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+// import styled from "styled-components"
+
+
+// const NavStyles = styled.div`
+//     background-color: red;
+// `
+
+export default function Nav() {
+    return (
+        <nav>
+            Hi! This is the Nav component!
+        </nav>
+    )
+}
